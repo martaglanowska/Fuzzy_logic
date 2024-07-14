4abf467fa66769cd80f6bba546d95ad9ec72fd85
martaglanowska <59265889+martaglanowska@users.noreply.github.com>
2024-07-14 15:22:21 +0300

    Functionality improved

--- fuzzy_model.py
+++ fuzzy_model.py
@@ -1,9 +1,22 @@
+class Input:
+
+  def __init__(self, values):     #input like: {"food": 2, "service": 4}
+    self.values = values
+    
+  def get_values(self):
+    return self.values
+  
+
 class FuzzySystem:
 
   def __init__(self, inputVariables, outputVariable, rules):
     self.inputVariables = inputVariables
     self.outputVariable = outputVariable
     self.rules = rules
+
+  def present_rules(self):
+    for r in self.rules:
+      print("Rule ", str(self.rules.index(r)+1), ":\t", r)
     
   def estimate(self, input):
     numerator = 0
@@ -12,14 +25,17 @@ class FuzzySystem:
       ruleActivationLevel = r.antecedent_authenticity(input)
       numerator += ruleActivationLevel * r.get_consequent()[0].get_sets()[r.get_consequent()[1]]
       denominator += ruleActivationLevel
-      print("activation level: ", ruleActivationLevel, "numerator: ", numerator, "denominator: ", denominator)
+      print("Rule ", str(self.rules.index(r)+1), " activation level:", ruleActivationLevel, ", numerator:", numerator, ", denominator:", denominator)
     return numerator/denominator
+  
+  def show_results(self, input):
+    print("\n-----RESULTS-----")
+    print("\nInput:\t", input.get_values(), "\n")
+    for r in self.rules:
+      print("Rule", str(self.rules.index(r)+1), "antecedent authenticity:", r.antecedent_authenticity(input))
+    print(' ')
+    print("\nValue suggested:\t", self.estimate(input), "\n")
 
 
-class Input:
 
-  def __init__(self, values):     #input like: {"food": 2, "service": 4}
-    self.values = values
-    
-  def get_values(self):
-    return self.values
+
