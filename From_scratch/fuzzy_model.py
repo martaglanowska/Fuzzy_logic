dfe4e0a8b444719205730c757555ee702e3f48f0
martaglanowska <59265889+martaglanowska@users.noreply.github.com>
2024-07-18 19:28:48 +0300

    Display improved

--- From_scratch/fuzzy_model.py
+++ From_scratch/fuzzy_model.py
@@ -31,10 +31,8 @@ class FuzzySystem:
   def show_results(self, input):
     print("\n-----RESULTS-----")
     print("\nInput:\t", input.get_values(), "\n")
-    for r in self.rules:
-      print("Rule", str(self.rules.index(r)+1), "antecedent authenticity:", r.antecedent_authenticity(input))
-    print(' ')
     print("\nValue suggested:\t", self.estimate(input), "\n")
+
 
 
 
